74ba96c8637f3973db2372dea20885e6db27d61a
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-12-31 20:20:46 +0000

    deleted smarttools - decimal seems to work

--- main.ts
+++ main.ts
@@ -1,11 +1,43 @@
+function Set_Volume () {
+    let l = control.createBuffer(7)
+l.setUint8(0, 0x7E)
+l.setUint8(1, 0x06)
+l.setUint8(2, 0x00)
+l.setUint8(3, 0x02)
+l.setUint8(4, 0x00)
+l.setUint8(5, Volume)
+l.setUint8(6, 0xEF)
+serial.writeBuffer(l)
+}
 input.onButtonPressed(Button.A, function () {
-    strip.setPixelColor(randint(0, 3), neopixel.colors(NeoPixelColors.Purple))
-    strip.show()
+    Set_Volume()
+    Track_Num += 1
+    if (Track_Num > 10) {
+        Track_Num = 1
+    }
+    basic.showString("" + Track_Num)
+    Play_Track()
 })
-let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-strip.showColor(neopixel.colors(NeoPixelColors.Red))
-strip.show()
+function Play_Track () {
+    let m = control.createBuffer(7)
+m.setUint8(0, 0x7E)
+m.setUint8(1, 0x03)
+m.setUint8(2, 0x00)
+m.setUint8(3, 0x02)
+m.setUint8(4, 0x00)
+m.setUint8(5, Track_Num)
+m.setUint8(6, 0xEF)
+serial.writeBuffer(m)
+}
+let Track_Num = 0
+serial.redirect(
+SerialPin.P2,
+SerialPin.P1,
+BaudRate.BaudRate9600
+)
+let Volume = 16
+Track_Num = 0
+basic.showIcon(IconNames.Rollerskate)
 basic.forever(function () {
 	
 })
